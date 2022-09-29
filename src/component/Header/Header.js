4c19c5c2038329ea8e3d7c612324a3015a89d593
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2022-09-29 12:09:46 +0600

    header  added

---
+++ src/component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+            <div className="mx-auto bg-dark text-white pt-2 pb-1">
+            <h1> WELCOME TO MUSCLE MONSTERS</h1>
+            <hr className='mx-5'/>
+        </div>
+    );
+};
+
+export default Header;
